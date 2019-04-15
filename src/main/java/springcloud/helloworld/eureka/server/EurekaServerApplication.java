6c3cd8e5c357e0fe6aa3cf99981b1cc92d21cfc1
insurancerb <insurancerbx@163.com>
2019-04-15 19:52:05 +0800

    Default Changelist

--- src/main/java/springcloud/helloworld/eureka/server/EurekaServerApplication.java
+++ src/main/java/springcloud/helloworld/eureka/server/EurekaServerApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+    private Logger logger = LoggerFactory.getLogger(EurekaServerApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class,args);
+        logger.info("eurekaServer starting");
     }
 }
