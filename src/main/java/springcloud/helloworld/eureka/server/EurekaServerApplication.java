736eb9febbdf1fa30be24221513bf1a73eab1f63
insurancerb <insurancerbx@163.com>
2019-04-17 09:04:20 +0800

    delete a file

--- src/main/java/springcloud/helloworld/eureka/server/EurekaServerApplication.java
+++ src/main/java/springcloud/helloworld/eureka/server/EurekaServerApplication.java
@@ -4,9 +4,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
-import java.util.logging.Logger;
-
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
